d38fe1a09bfe2bcb192284148b223042943642ac
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2017-11-26 20:53:58 -0300

    Add all required params in bin py

--- bin/sbrain-cli.py
+++ bin/sbrain-cli.py
@@ -4,66 +4,74 @@
 
 from __future__ import print_function
 from sbrain import __version__
-from gtts import gTTS
+from sbrain.engine import Builder
 from os import *
+
 
 import sys
 import argparse
-import codecs
-import re
-import subprocess
-import shutil
 import os
 # Args
 desc = "Creates a video with text and pronunciation ({v})".format(v=__version__)
 parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.RawTextHelpFormatter)
 
-text_group = parser.add_mutually_exclusive_group(required=True)  
-text_group.add_argument('-f', '--file', help="file text")
+#text_group = parser.add_mutually_exclusive_group(required=True)  
+
+parser.add_argument('-f', '--file', help="file text")
+parser.add_argument('-l', '--language', help="language text")
+parser.add_argument('-t', '--translate', help="translate")
+parser.add_argument('-tr', '--translation-repeat', help="repeat translate")
+parser.add_argument('-r', '--repeat', help="repeat language")
+parser.add_argument('-key', '--api_key', help="api key google")
+parser.add_argument('--no-time',  help="time")
+parser.add_argument('-o', '--output', help="output file")
 
 parser.add_argument('--debug', action="store_true")
+parser.add_argument('--random', help='Disable random generator')
+
+
+if len(sys.argv) == 1:
+	parser.print_help()
+	sys.exit(1)
 
 args = parser.parse_args()
 
-
+b = Builder()
 
 try:
-    if args.file:
+
+	if args.file:
+		b.file = args.file
+
+	if args.language:
+		b.language = args.language
+
+	if args.translate:
+		b.translate = args.translate
+
+	if args.translation_repeat:
+		b.translation_repeat = int(args.translation_repeat)
+
+	if args.api_key:
+		b.api_key_google = args.api_key
+
+	if args.repeat:
+		b.repeat = int(args.repeat)
+
+	if args.no_time:
+		b.time = False
+
+	if args.output:
+		b.output = args.output
+
+	if args.random:
+		b.random = True
 
 
-        with codecs.open(args.file, 'r', 'utf-8') as ft:
-            text = ft.read()
-
-        delimiters = '.', '...', '?', '!'
-        regexPattern = '|'.join(map(re.escape, delimiters))
-        lista = re.split(regexPattern, text.replace('\n',''))
-        
-        concat = 'concat:'
-
-
-        if not os.path.exists('temp'):
-            os.makedirs('temp')
-
-        for idx, val in enumerate(lista):
-
-            nameFile = 'temp/'+ str(idx)+'.mp3'
-            tts = gTTS(text=val, lang='en')
-            tts.save(nameFile)
-
-            if idx > 0:
-                concat += '|'
-
-            concat += nameFile
-
-
-        #ffmpeg -i 'concat:input1|input2' -codec copy output
-        subprocess.call(['ffmpeg','-i', concat, '-codec', 'copy','output.mp3'])
-
-        if os.path.exists('temp'):
-            shutil.rmtree('temp')
-
-
+	
+	b.generate_audio()
 
     
 except Exception as e:
+
     print("ERROR: ", e, file=sys.stderr)
